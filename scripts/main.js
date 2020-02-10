a75c64cbb9a41c0069e29514356f6f3667f33b82
Hector Lopez <hlopezcarballo@gmail.com>
2020-02-10 04:42:52 -0600

    added input readers

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
-const start_game = document.getElementById('start-sign')
+const start_game_sign = document.getElementById('start-sign')
 const red = document.getElementById('red')
 const blue = document.getElementById('blue')
 const yellow = document.getElementById('yellow')
 const green = document.getElementById('green')
+const FINAL_LEVEL = 20
 
-start_game.addEventListener('click', startGame)
+start_game_sign.addEventListener('click', startGame)
 
 class Juego {
   constructor() {
     this.initialize()
     this.generateSequence()
     this.nextLevel()
+    
   }
   initialize() {
-    start_game.classList.add('is-active')
+    this.toggleStartSign()
+    this.checkColor = this.checkColor.bind(this)
+    this.nextLevel = this.nextLevel.bind(this)
     this.level = 7
     this.colores = {
       red: red,
@@ -22,8 +26,15 @@ class Juego {
       green
     }
   }
+  toggleStartSign() {
+    if (start_game_sign.classList.contains('is-active')) {
+      start_game_sign.classList.remove('is-active')
+    } else {
+      start_game_sign.classList.add('is-active')
+    }
+  }
   generateSequence() {
-    this.secuencia = new Array(10).fill(0).map( n => Math.floor(Math.random() * 4))
+    this.secuencia = new Array(FINAL_LEVEL).fill(0).map( n => Math.floor(Math.random() * 4))
   }
   changeNumberToColor(number) {
     switch (number) {
@@ -37,14 +48,32 @@ class Juego {
         return 'green'
     }
   }
+  changeColorToNumber(color) {
+    switch (color) {
+      case 'red':
+        return 0
+      case 'blue':
+        return 1
+      case 'yellow':
+        return 2
+      case 'green':
+        return 3
+    }
+  }
+
   nextLevel() {
     this.lightSequence()
+    this.sublevel = 0
+
   }
+ 
   lightSequence() {
     for (let i = 0; i < this.level; i++) {
       const color = this.changeNumberToColor(this.secuencia[i])
       setTimeout(() => this.lightColor(color), 1000 * i ) 
     }
+    this.addListener()
+
   }
   lightColor (color) {
     this.colores[color].classList.add('light')
@@ -52,6 +81,38 @@ class Juego {
   }
   removeColor(color) {
     this.colores[color].classList.remove('light')
+  }
+
+  addListener() {
+    this.colores.red.addEventListener('click', this.checkColor)
+    this.colores.blue.addEventListener('click', this.checkColor)
+    this.colores.yellow.addEventListener('click', this.checkColor)
+    this.colores.green.addEventListener('click', this.checkColor)
+  }
+  removeListener() {
+    this.colores.red.removeEventListener('click', this.checkColor)
+    this.colores.blue.removeEventListener('click', this.checkColor)
+    this.colores.yellow.removeEventListener('click', this.checkColor)
+    this.colores.green.removeEventListener('click', this.checkColor)
+  }
+  checkColor(ev) {
+    const color_name = ev.target.dataset.color
+    this.lightColor(color_name)
+    const color_number = this.changeColorToNumber(color_name)
+    if (this.secuencia[this.sublevel] === color_number) {
+      this.sublevel++
+      if (this.sublevel === this.level){
+        this.level++
+        this.removeListener()
+        if (this.level === FINAL_LEVEL + 1) {
+          //gano
+        } else {
+        setTimeout(this.nextLevel, 2000) 
+        }
+      }
+    } else {
+      //perdio
+    }
   }
 }
 
