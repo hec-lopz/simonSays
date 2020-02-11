f28e9f5abffa368292a26bb86eecb2993ac3d565
Hector Lopez <hlopezcarballo@gmail.com>
2020-02-11 02:29:44 -0600

    Finished Main Game

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const red = document.getElementById('red')
 const blue = document.getElementById('blue')
 const yellow = document.getElementById('yellow')
 const green = document.getElementById('green')
-const FINAL_LEVEL = 20
+const FINAL_LEVEL = 5
 
 start_game_sign.addEventListener('click', startGame)
 
@@ -12,13 +12,15 @@ class Juego {
     this.initialize()
     this.generateSequence()
     this.nextLevel()
-    
+    console.log(this.secuencia)
   }
   initialize() {
     this.toggleStartSign()
     this.checkColor = this.checkColor.bind(this)
     this.nextLevel = this.nextLevel.bind(this)
-    this.level = 7
+    this.addListener = this.addListener.bind(this)
+    this.initialize = this.initialize.bind(this)
+    this.level = 1
     this.colores = {
       red: red,
       blue,
@@ -68,52 +70,78 @@ class Juego {
   }
  
   lightSequence() {
+    console.log(this.secuencia)
     for (let i = 0; i < this.level; i++) {
       const color = this.changeNumberToColor(this.secuencia[i])
       setTimeout(() => this.lightColor(color), 1000 * i ) 
     }
-    this.addListener()
+    setTimeout(this.addListener, this.level * 800 ) 
 
   }
   lightColor (color) {
     this.colores[color].classList.add('light')
-    setTimeout(() => this.removeColor(color), 500 )
+    setTimeout(() => this.removeColor(color), 400 )
   }
   removeColor(color) {
     this.colores[color].classList.remove('light')
   }
 
   addListener() {
+    console.log('addListener engaged')
     this.colores.red.addEventListener('click', this.checkColor)
     this.colores.blue.addEventListener('click', this.checkColor)
     this.colores.yellow.addEventListener('click', this.checkColor)
     this.colores.green.addEventListener('click', this.checkColor)
   }
   removeListener() {
+    console.log('removeListener engaged')
     this.colores.red.removeEventListener('click', this.checkColor)
     this.colores.blue.removeEventListener('click', this.checkColor)
     this.colores.yellow.removeEventListener('click', this.checkColor)
     this.colores.green.removeEventListener('click', this.checkColor)
   }
   checkColor(ev) {
+    // debugger
+    console.log('btnPressed')
     const color_name = ev.target.dataset.color
     this.lightColor(color_name)
     const color_number = this.changeColorToNumber(color_name)
+    console.log(color_number)
     if (this.secuencia[this.sublevel] === color_number) {
       this.sublevel++
       if (this.sublevel === this.level){
         this.level++
         this.removeListener()
         if (this.level === FINAL_LEVEL + 1) {
-          //gano
+          this.gameWon()
         } else {
-        setTimeout(this.nextLevel, 2000) 
+        setTimeout(this.nextLevel, 1300) 
         }
       }
     } else {
-      //perdio
+      this.gameLost()
+      this.removeListener()
+
     }
   }
+  gameWon() {
+    swal({
+      title: '¡Felicidades!',
+      text: 'Ganaste el juego',
+      icon: 'success',
+      button: '¡I love this!'
+
+    }).then(this.initialize)
+  } 
+  gameLost() {
+    swal({
+      title: 'Rayos...',
+      text: 'Perdiste el juego. :c',
+      icon: 'error',
+      button: 'Intentar de nuevo'
+
+    }).then(this.initialize)
+  } 
 }
 
 
